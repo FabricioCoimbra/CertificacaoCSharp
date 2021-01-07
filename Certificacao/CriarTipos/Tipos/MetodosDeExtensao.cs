aa37f223467b34bb1f4bb704b5029ab03d89b466
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2021-01-07 19:14:07 -0300

    Porpriedades indexadas

---
+++ Certificacao/CriarTipos/Tipos/MetodosDeExtensao.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriarTipos.Tipos
+{
+    class MetodosDeExtensao : IAulaItem
+    {
+        public void Executar()
+        {
+            Impressora impressora = new Impressora("Este é\r\no meu documento");
+            impressora.ImprimirDocumento();
+            impressora.ImprimirDocumentoHTML();
+            impressora.ImprimirDocumentoComResumo();
+        }        
+    }
+    class Impressora
+    {
+        public string Documento { get; }
+        public Impressora(string documento)
+        {
+            this.Documento = documento;
+        }
+        public void ImprimirDocumento()
+        {
+            Console.WriteLine();
+            Console.WriteLine(Documento);
+        }
+    }
+
+    static class ImpressoraExtensions
+    {
+        public static void ImprimirDocumentoHTML(this Impressora impressora)
+        {
+            Console.WriteLine($"<html><body>{impressora.Documento}</body></html>");
+        }
+
+        public static void ImprimirDocumentoComResumo(this Impressora impressora)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{impressora.Documento}\r\nRESUMO\r\n======\r\nO documento tem: {impressora.Documento.Length} caracteres.");
+        }
+    }
+}
