f0c09bda65197d11e60c69347030bfcd6e735f56
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2021-01-07 20:50:53 -0300

    Conversao explicita de um double com capacidade insana de valores possíveis

---
+++ Certificacao/ConsumirTipos/ConsumirTipos/ConversoesExplicitas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsumirTipos.ConsumirTipos
+{
+    class ConversoesExplicitas : IAulaItem
+    {
+        public void Executar()
+        {
+            Console.WriteLine("long.MaxValue : {0}", long.MaxValue);
+            Console.WriteLine("double.MaxValue : {0}", double.MaxValue);
+            double divida = (double)long.MaxValue * 5;
+            try
+            {
+                long copia = (long)divida;
+                Console.WriteLine("Copia ficou com o maior valor possivel de long sem dar erro: {0}", copia);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Double muito grande não cabe no long");
+            }           
+
+            Console.WriteLine(divida);
+
+            double salario = 1_237.89;
+            long copiaSalario = (long)salario;
+            Console.WriteLine(copiaSalario);
+
+            Animal animal = new Gato();
+            Gato gato = (Gato)animal; //cast
+            Console.WriteLine(gato.GetType());
+        }
+    }
+}
--- Certificacao/ConsumirTipos/Program.cs
+++ Certificacao/ConsumirTipos/Program.cs
@@ -66,7 +66,7 @@ namespace ConsumirTipos
             {                
                 new MenuItem("Boxing e Unboxing", typeof(Boxing)),
                 new MenuItem("Conversoes Implícitas", typeof(ConversoesImplicitas)),
-                //new MenuItem("Conversoes Explícitas", typeof(ConversoesExplicitas)),
+                new MenuItem("Conversoes Explícitas", typeof(ConversoesExplicitas)),
                 //new MenuItem("Operadores IS e AS", typeof(OperadoresISeAS)),
                 //new MenuItem("Operadores de Conversão", typeof(OperadoresDeConversao)),
                 //new MenuItem("Métodos Auxiliares de Conversão", typeof(MetodosAuxiliares)),
