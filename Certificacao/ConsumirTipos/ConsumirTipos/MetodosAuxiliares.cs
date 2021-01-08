1c51fb66b0227cbd47a102ad1aa8d0bc0ab0162f
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2021-01-07 21:31:59 -0300

    utilizando Metodos Auxiliares de conversão

---
+++ Certificacao/ConsumirTipos/ConsumirTipos/MetodosAuxiliares.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsumirTipos.ConsumirTipos
+{
+    class MetodosAuxiliares : IAulaItem
+    {
+        public void Executar()
+        {
+            string textoDigitado = "123";
+            //int numeroConvertido = textoDigitado;
+
+            int numeroConvertido = int.Parse(textoDigitado);
+            Console.WriteLine(numeroConvertido);
+
+            textoDigitado = "abc";
+            if (int.TryParse(textoDigitado, out numeroConvertido))
+            {
+                Console.WriteLine(numeroConvertido);
+            }
+            else
+            {
+                Console.WriteLine("texto não é um numero");
+            }
+
+            textoDigitado = "123,45";
+
+            decimal.TryParse(textoDigitado, out decimal valorConvertido);
+            Console.WriteLine(valorConvertido);
+
+            textoDigitado = "R$ 123,45";
+            decimal.TryParse(textoDigitado,
+                        System.Globalization.NumberStyles.Currency, //moeda
+                        System.Globalization.CultureInfo.CurrentCulture, //pt-BR
+                        out valorConvertido);
+
+            Console.WriteLine(valorConvertido);
+        }
+    }
+}
