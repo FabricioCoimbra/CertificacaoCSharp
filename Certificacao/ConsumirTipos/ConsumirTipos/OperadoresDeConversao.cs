ad905440718c8e76516352b9cbb89df7ce3ea5b5
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2021-01-07 21:08:02 -0300

    Conversão comum de angulos em graus e radianos.

---
+++ Certificacao/ConsumirTipos/ConsumirTipos/OperadoresDeConversao.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsumirTipos.ConsumirTipos
+{
+    class OperadoresDeConversao : IAulaItem
+    {
+        public void Executar()
+        {
+            AnguloEmGraus anguloEmGraus = AnguloEmGraus.Converte(45);
+            Console.WriteLine(anguloEmGraus);
+
+            AnguloEmRadianos anguloEmRadianos = AnguloEmRadianos.Converte(15);
+            Console.WriteLine(anguloEmRadianos);
+
+            double graus = AnguloEmGraus.Converte(anguloEmGraus);
+            Console.WriteLine(graus);
+
+            anguloEmRadianos = AnguloEmRadianos.Converte(anguloEmGraus);
+            anguloEmGraus = AnguloEmGraus.Converte(anguloEmRadianos);
+            System.Console.WriteLine($"anguloEmGraus: {anguloEmGraus}");
+            System.Console.WriteLine($"anguloEmRadianos: {anguloEmRadianos}");
+        }
+    }
+    public struct AnguloEmRadianos
+    {
+        public double Radianos { get; }
+
+        public AnguloEmRadianos(double radianos)
+        {
+            this.Radianos = radianos;
+        }
+
+        public static AnguloEmRadianos Converte(AnguloEmGraus graus)
+        {
+            return new AnguloEmRadianos(graus.Graus * System.Math.PI / 180);
+        }
+
+        public static AnguloEmRadianos Converte(double radianos)
+        {
+            return new AnguloEmRadianos(radianos);
+        }
+
+        public static double Converte(AnguloEmRadianos radianos)
+        {
+            return radianos.Radianos;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} radianos", this.Radianos);
+        }
+    }
+
+    public struct AnguloEmGraus
+    {
+        public double Graus { get; }
+        public AnguloEmGraus(double graus) { this.Graus = graus; }
+        public static AnguloEmGraus Converte(AnguloEmRadianos radianos)
+        {
+            return new AnguloEmGraus(radianos.Radianos * 180 / System.Math.PI);
+        }
+        public static AnguloEmGraus Converte(double graus)
+        {
+            return new AnguloEmGraus(graus);
+        }
+        public static double Converte(AnguloEmGraus graus)
+        {
+            return graus.Graus;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} graus", this.Graus);
+        }
+    }
+}
--- Certificacao/ConsumirTipos/Program.cs
+++ Certificacao/ConsumirTipos/Program.cs
@@ -68,7 +68,7 @@ namespace ConsumirTipos
                 new MenuItem("Conversoes Implícitas", typeof(ConversoesImplicitas)),
                 new MenuItem("Conversoes Explícitas", typeof(ConversoesExplicitas)),
                 new MenuItem("Operadores IS e AS", typeof(OperadoresISeAS)),
-                //new MenuItem("Operadores de Conversão", typeof(OperadoresDeConversao)),
+                new MenuItem("Operadores de Conversão", typeof(OperadoresDeConversao)),
                 //new MenuItem("Métodos Auxiliares de Conversão", typeof(MetodosAuxiliares)),
                 //new MenuItem("Usando Dynamic", typeof(UsandoDynamic)),
                 //new MenuItem("Conversões de Dynamic", typeof(ConversoesDeDynamic)),
