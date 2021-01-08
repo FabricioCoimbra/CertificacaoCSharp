15c34bdea52775995b5992142b741492f2418b31
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2021-01-08 17:22:18 -0300

    Encapsulamento simples da propriedade salário

---
+++ Certificacao/AplicarEncapsulamento/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AplicarEncapsulamento
+{
+    class Funcionario
+    {
+        private decimal salario;
+
+        public decimal Salario
+        {
+            get { return salario; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("O salário não pode ser negativo");
+                }
+                salario = value;
+            }
+        }
+
+    }
+}
---
+++ Certificacao/AplicarEncapsulamento/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AplicarEncapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            funcionario.Salario = 1000;
+
+            Console.WriteLine(funcionario.Salario);
+
+            funcionario.Salario = -1000;
+            Console.WriteLine(funcionario.Salario);
+
+        }
+    }
+}
