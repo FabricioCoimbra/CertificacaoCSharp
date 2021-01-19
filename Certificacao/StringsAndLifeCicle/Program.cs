2698c08cb4d660f4448ba4d03b246a4b3117d5f0
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2021-01-18 22:18:05 -0300

    Criando novo projeto de controle de ciclo de vida e atualizando para .net 5.0

---
+++ Certificacao/StringsAndLifeCicle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringsAndLifeCicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
